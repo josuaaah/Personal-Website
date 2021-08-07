a7f8ea6f7755f145e3e99974e426a1b55b294f02
Joshua Chew <Josuaaah@Joshuas-MacBook-Pro.local>
2021-08-07 11:21:45 +0800

    Add static image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Joshua Chew Jian Xiang's Personal Website",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>NUS Year 3 Computer Science student, Minor in Statistics, University Scholars Programme</p>
+      <StaticImage
+        alt="A software engineer hard at work."
+        src="https://thumbs.dreamstime.com/b/portrait-his-nice-attractive-focused-brunette-guy-creating-script-coding-java-html-mysql-database-using-languages-software-161965647.jpg"
+      />
     </Layout>
   )
 }

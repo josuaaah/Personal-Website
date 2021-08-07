878bc8f7674caffa34e4137a611fa8daf3c637b6
Joshua Chew <Josuaaah@Joshuas-MacBook-Pro.local>
2021-08-07 10:38:42 +0800

    Create page components

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I am a passionate Computer Science student with an interest in Artificial Intelligence and Computer Security.</p>
+      <p>I am also interested in fantasy novels, music covers and software engineering.</p>
+    </main>
+  )
+}
+
+export default AboutPage

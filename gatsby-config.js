273fa3dd95239843d709d83847ca7859c891c382
Joshua Chew <Josuaaah@Joshuas-MacBook-Pro.local>
2021-08-07 10:06:41 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Joshua Chew Jian Xiang's Personal Website",
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud"],
+};

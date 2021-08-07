39c1a23b9ee72d37962c5b848661d5511aa14643
Joshua Chew <Josuaaah@Joshuas-MacBook-Pro.local>
2021-08-07 10:43:43 +0800

    Use the Link component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>Hi there! I am a passionate Computer Science student with an interest in Artificial Intelligence and Computer Security.</p>
       <p>I am also interested in fantasy novels, music covers and software engineering.</p>
     </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <main>
       <title>Josuaaah</title>
       <h1>Joshua Chew's Personal Website</h1>
+      <Link to="/about">About</Link>
       <p>NUS Year 3 Computer Science student, Minor in Statistics, University Scholars Programme</p>
     </main>
   )

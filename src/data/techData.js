141e350cbc109b8e375a9b202490113de729d549
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2022-05-22 14:07:05 +0800

    Add tech stack for 3E Accounting

--- src/data/techData.js
+++ src/data/techData.js
@@ -1,7 +1,25 @@
+import AngularJsLogo from '../images/angularjs_logo.png'
+import DjangoLogo from '../images/django_logo.png'
+import MySqlLogo from '../images/mysql_logo.png'
 import PumlLogo from '../images/puml_logo.png'
 import PygameLogo from '../images/pygame_logo.png'
 import PythonLogo from '../images/python_logo.png'
 import RubyOnRailsLogo from '../images/ruby_on_rails_logo.png'
+
+export let djangoIcon = {
+  name: "Django",
+  src: DjangoLogo,
+};
+
+export let angularJsIcon = {
+  name: "AngularJS",
+  src: AngularJsLogo,
+};
+
+export let mySqlIcon = {
+  name: "MySQL",
+  src: MySqlLogo,
+};
 
 export let pygameIcon = {
   name: "Pygame",

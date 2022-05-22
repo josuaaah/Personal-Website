7f35310f6a14bfaff08400f91f68162dd7598d81
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2022-05-22 12:43:10 +0800

    Update indexData.js

--- src/data/indexData.js
+++ src/data/indexData.js
@@ -5,11 +5,10 @@ const displayText = `Hello. I am Joshua Chew.`;
 const descriptionText = `
   I am a third-year Computer Science student at the National University of Singapore.
   I specialize in Artificial Intelligence, am pursuing a Second Major in Statistics, 
-  and am also part of the University Scholar's Programme (USP). I am passionate in 
-  creating things - literally anything!
-
-  I also have a soft spot for fantasy novels, and I used to make music covers a lot 
-  before I got busy. Hope we can get in touch soon!`;
+  and am also part of the University Scholar's Programme (USP). A perk of having me 
+  as a software developer: I write very, very, very clean code. Outside of Computing, 
+  I have a soft spot for fantasy novels, and I used to make music covers before I got busy. 
+  Hope we have a chance to connect soon!`;
 
 export let myPhoto = MyPhoto;
 export let myDisplayText = displayText;
